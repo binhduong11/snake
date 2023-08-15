d9f9ced9aaa843b77f321b0ee27c13fc934cc952
binhduong11 <nguyenbinhduongcavoi@gmail.com>
2023-08-16 02:41:19 +0700

    cach2

--- src/test_02/Main.java
+++ src/test_02/Main.java
@@ -22,7 +22,7 @@ public class Main {
         System.out.println("y = ");
         int YKetThuc = scanner.nextInt();
         ToaDo KetThuc = new ToaDo("Kết thúc", XKetThuc, YKetThuc);
-        
+
         // Nhập số lượng thức ăn và thông tin tọa độ
         System.out.println("Nhập số lượng thức ăn");
         int soluong = scanner.nextInt();
@@ -33,43 +33,113 @@ public class Main {
             int x = scanner.nextInt();
             System.out.println("y = ");
             int y = scanner.nextInt();
-            ToaDo itemp = new ToaDo("thức ăn số " + i, x, y);
+            int ten = i+1;
+            ToaDo itemp = new ToaDo("thức ăn số " + ten, x, y);
 
             z.add(itemp);
 
         }
-        
-        var sapxep = listToaDo(batdau, KetThuc, z);
-        
-        System.out.println("Đường đi ngắn nhất là :");
-        for (int i = 0; i < sapxep.size(); i++) {
-            System.out.println(sapxep.get(i).getTen() + " ==> ");
+        var TatatCaDuongDi = taoHoanVi(z);
+        System.out.println("Có tât cả: " + z.size() + "! cách đi:");
+        for(List<ToaDo> ab : TatatCaDuongDi){
+            ab.add(KetThuc);
+            ab.add(0, batdau);
+            for (ToaDo ac : ab) {
+                System.out.print( ac.getTen()  );
+            }
+            System.out.print("Tổng đường đi = " + QuangDuong(ab));
+            System.out.println("");
         }
+        List<ToaDo> duongDiNganNhat = DuongDiNganNhat(taoHoanVi(z),batdau,KetThuc);
+         
+        //var sapxep = listToaDo(batdau, KetThuc, z);
 
-        for (int i = 0; i < sapxep.size() - 1; i++) {
-            sapxep.get(i).DuongDi(sapxep.get(i + 1));
+        System.out.println("Đường đi ngắn nhất là :");
+        for (int i = 0; i < duongDiNganNhat.size(); i++) {
+            System.out.println(duongDiNganNhat.get(i).getTen() + " ==> ");
+        }
+        System.out.println("Tổng đường đi = " + QuangDuong(duongDiNganNhat));
+        System.out.println("chitiet: ");
+        for (int i = 0; i < duongDiNganNhat.size() - 1; i++) {
+            duongDiNganNhat.get(i).DuongDi(duongDiNganNhat.get(i + 1));
         }
     }
 
-    // hàm tìm đường đi ngắn nhất bằng phương pháp so sánh khoảng cách từ điểm xuất phát đến điểm lân cận gần nhất 
-    public static List<ToaDo> listToaDo(ToaDo batdau, ToaDo ketthuc, List<ToaDo> listtoado) {
-                     
-        listtoado.add(0, batdau);
+    public static int QuangDuong(List<ToaDo> x) {
+        int quangduong = 0;
+        for (int i = 0; i < x.size() - 1; i++) {
+            quangduong +=  x.get(i).KhoangCach(x.get(i + 1));
+        }
+        return quangduong;
+    }
+// tìm ra đường đi ngắn nhất trong tất cả 
+    public static List<ToaDo> DuongDiNganNhat(List<List<ToaDo>> hoanvi, ToaDo BatDau, ToaDo KetThuc) {
+        for (List<ToaDo> list : hoanvi) {
+            list.add(0, BatDau);
+            list.add(KetThuc);
+        }
+        List<ToaDo> min = new ArrayList<>(hoanvi.get(0)); 
+        for (int i = 0; i < hoanvi.size(); i++) {
+            if (QuangDuong(hoanvi.get(i)) < QuangDuong(min)) {               
+                min = new ArrayList<>(hoanvi.get(i)); 
+            }
+        }
+        return min;
+    }
+// Tạo danh sách tất cả hoán vị của đường đi
+    public static List<List<ToaDo>> taoHoanVi(List<ToaDo> toadoList) {
+        List<List<ToaDo>> hoanVi = new ArrayList<>();
+        taoHoanVi(toadoList, new ArrayList<>(), hoanVi);
+        return hoanVi;
+    }
+
+    // Hàm đệ quy để tạo hoán vị
+    private static void taoHoanVi(List<ToaDo> conLai, List<ToaDo> hienTai, List<List<ToaDo>> hoanVi) {
+
+        if (conLai.isEmpty()) {
+            hoanVi.add(new ArrayList<>(hienTai)); 
+            return;
+        }
+
         
+        for (int i = 0; i < conLai.size(); i++) {
+            ToaDo so = conLai.get(i); 
+            hienTai.add(so); 
+            conLai.remove(i); 
+
+            
+            taoHoanVi(conLai, hienTai, hoanVi);
+
+            conLai.add(i, so); 
+            hienTai.remove(hienTai.size() - 1); 
+        }
+    }
+    
+    
+// cách cũ
+    // hàm tìm đường đi ngắn nhất bằng phương pháp so sánh khoảng cách từ điểm xuất phát đến điểm lân cận gần nhất 
+    //  B1: Lặp qua từng điểm trong danh sách
+    //  B2: Trong mỗi vòng lặp, lặp qua các điểm còn lại trong danh sách
+    //  B3: So sánh khoảng cách giữa điểm đang xét i đến i +1 với khoảng cách giữa điểm đang xét i và điểm sau nó trong danh sách j
+    // nếu khoảng cách giữa điểm đang xét i đến đến j < khoảng cách giữa i đến i +1 thì đổi vị trí của j với i+1 
+    public static List<ToaDo> listToaDo(ToaDo batdau, ToaDo ketthuc, List<ToaDo> listtoado) {
+
+        listtoado.add(0, batdau);
+
         // Sử dụng thuật toán sắp xếp nổi bọt
         for (int i = 0; i < listtoado.size() - 1; i++) {
-            for (int j = i + 1; j <= listtoado.size() - 1 ; j++) {
-                
+            for (int j = i + 1; j <= listtoado.size() - 1; j++) {
+
                 // so sánh d(i,j) với d(i,i+1) nếu phát hiện khoảng cách d(i,j) < thì đổi chỗ i với j =< kết thúc vòng lặp thứ 2
                 // điểm có khoảng cách gần nhất sẽ ở vị trí i +1 , tiếp tục so sánh đến khi kết thúc vòng lặp thứ nhất
-                if ( listtoado.get(i).KhoangCach(listtoado.get(j)) <  listtoado.get(i).KhoangCach(listtoado.get( i +1))) {
+                if (listtoado.get(i).KhoangCach(listtoado.get(j)) < listtoado.get(i).KhoangCach(listtoado.get(i + 1))) {
                     ToaDo min = listtoado.get(j);
-                    listtoado.set(j, listtoado.get(i +1));
-                    listtoado.set(i +1, min); 
+                    listtoado.set(j, listtoado.get(i + 1));
+                    listtoado.set(i + 1, min);
                 }
 
             }
-                       
+
         }
         listtoado.add(ketthuc);
         return listtoado;
